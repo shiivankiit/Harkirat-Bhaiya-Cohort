2dd7e5ea3f7ce0c11447dcca9fded37806e0a521
shiivankiit <shivankit7557@gmail.com>
2025-09-03 01:12:00 +0530

    Revised the lect and added some commnet for code redability.

--- Javascript/week2.2/03Promise.js
+++ Javascript/week2.2/03Promise.js
@@ -1,9 +1,11 @@
 //Promise class geaves you a promise that i will return you something in a future.
 
-//Explanation: A promise expects a function that prfrm an actual asynchronous task.
+//Defination-: A promise expects a function that prfrm an actual asynchronous task.
 // Once the async task is complete,call the argumnet of that function, passing in the
 //data obtained from the async task.
 //That data will then passed to the function you define in`.then`.
+
+//Topic-01
 function name()
 {
     console.log("It's harkirat chohort");
@@ -14,9 +16,7 @@ setTimeout(name,3000); // setTimeout promise you to return something after 3 sec
 
 // A promise in javascript is an object that represent the eventual completition (or failure) of an
 // asynchronous operation and its resulting value.
-
-
-function main()//calling the main function is eventual completion of settimeout asynchronous operation.
+function main()//calling the main function is representing the eventual completion of settimeout(asynchronous operation).
 {
  console.log("Task done");
  
@@ -39,6 +39,7 @@ function setTimeoutPromisifed(ms){
     // ✅ So we return a Promise from this function.
     
 }
+//Topic-02.
 function callback(){
     console.log("3 second have passed");
 }
@@ -52,7 +53,7 @@ setTimeoutPromisifed(3000).then(callback)
 function setTimeoutPromisifed(ms){
     return new Promise(); //return instance of the promise class.
 }
-let pp= setTimeoutPromisifed(5000);//pp is object of promsie class
+let pp= setTimeoutPromisifed(5000);//returns you the object of promsie class.
 console.log(p);
 
 //1.18.30
@@ -71,7 +72,7 @@ promiseCallback(main);
 //You are passing the main function as an argument to promiseCallback.
 //Inside promiseCallback(resolve), the parameter resolve now refers to the function main.
 //passing main as an argument to promiseCallback, and that main becomes the resolve parameter.
-//so it is same as setTimeout(main,3000);
+//so it is same as setTimeout(main,3000).
 //first we call the function promiseCallback when it get resolve after 3 sec.
 //then we log the content of main function.
 
@@ -82,20 +83,24 @@ function waitfor3S(resolve){//this resolve is automatically provided by promise 
 }
 function setTimeoutPromisifed(){
     return new Promise(waitfor3S);//waitfor3s is executor function of the promise.
+    //Promise class says i will take one function as an input.what  ever the first argument of that  function called
+    //resolve ...call .then() function whatever the passed inside .then().
+    
 }
 function main(){
     console.log("main is called");
 }
 setTimeoutPromisifed().then(main);//promise based approach.
 
-//return new Promise(waitfor3s):Promise class says i will take one function as an input
+//return new Promise(waitfor3s):Promise class says i will take one function as an input. 
 //and the first argument of that function when call ..then we call .then()function.
 
                                     //next function
 function random(){
 
 }
-let p = new Promise(random);//supposed to return u something eventually. 
+let p = new Promise(random);//supposed to return u something eventually return you a instance of Promise class. 
+//Agar tum promise class bana rahe ho to tumhe ye bhi batana ha ki how this promise complete.
 function callback(){
     console.log("promise succeded");  
 }
@@ -107,7 +112,7 @@ console.log(p);
 
 function random(resolve){ //resolve is also a function provided by promise constructor.
     setTimeout(resolve,3000);//call resolve after 3sec.
-}//when you call resolve you are saying promise is fullfiled
+}//when you call resolve you are saying promise is fullfiled.
 const a = new Promise (random);//supposed to return u something eventually. 
 // a is now a Promise that resolves after 3 sec.
 
