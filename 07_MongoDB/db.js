81c68af9d1d311925962a0190db2e73835d10337
shiivankiit <shivankit7557@gmail.com>
2025-10-07 23:48:20 +0530

    Comment added in the code

--- 07_MongoDB/db.js
+++ 07_MongoDB/db.js
@@ -1,14 +1,15 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose');//This library let you create schemas.and schema is final structure of the database.
+const Schema = mongoose.Schema;//Here mongoose define a class callled schema.
 const ObjectId = mongoose.ObjectId;
 
+//Using schema we define the structure in database how data goes in database.
 const User = new Schema({
     email:String,
     password:String,
     name:String
 })
 
-const Todo = new Schema({
+const Todo = new Schema({ //When i am storing something to todo i also store userId i.e which user it belong to.
      title:String,
      done:Boolean,
      userId:ObjectId
@@ -17,7 +18,7 @@ const Todo = new Schema({
 //Mongoose.model says in which collection you want to insert the data.
 //In this case we are putting the data into usrs collection.
 //Basically this model let you insert the data in users collection{users,todos}.
-
+//Here we are defining model.
 const UserModel = mongoose.model('users',User);
 const TodoModel = mongoose.model('todos',Todo);
 
